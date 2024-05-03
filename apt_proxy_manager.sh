720992ad8ea0104554a6d77d96f8f62ff1754b35
manastria <manastria@users.noreply.github.com>
2024-05-03 18:26:27 +0200

    Update apt_proxy_manager.sh script to include backup proxies

--- apt_proxy_manager.sh
+++ apt_proxy_manager.sh
@@ -4,6 +4,12 @@
 DEFAULT_IP="192.168.1.100" # Remplacez cette adresse par celle de votre serveur apt-cacher-ng par défaut
 DEFAULT_PORT=3142
 
+# Liste des proxies de secours (IP:port)
+BACKUP_PROXIES=(
+    "172.16.0.1:3128"
+    "172.16.0.1:3128"
+)
+
 # Fonction pour configurer le fichier de proxy APT
 configure_apt_proxy() {
     local ip=$1
@@ -23,14 +29,27 @@ configure_apt_proxy() {
     # Configuration du proxy auto-detect
     echo "Acquire::http::Proxy-Auto-Detect \"/usr/local/bin/apt-proxy-detect.sh\";" > /etc/apt/apt.conf.d/00aptproxy
 
-    # Script de détection du proxy
+    # Création ou mise à jour du script de détection du proxy
     cat <<EOF > /usr/local/bin/apt-proxy-detect.sh
 #!/bin/bash
+# Test du proxy principal
 if nc -w1 -z $ip $port; then
     echo -n "http://$ip:$port"
-else
-    echo -n "DIRECT"
+    exit 0
 fi
+
+# Test des proxies de secours
+for proxy in "${BACKUP_PROXIES[@]}"; do
+    proxy_ip=\${proxy%:*}
+    proxy_port=\${proxy#*:}
+    if nc -w1 -z \$proxy_ip \$proxy_port; then
+        echo -n "http://\$proxy_ip:\$proxy_port"
+        exit 0
+    fi
+done
+
+# Si aucun proxy n'est disponible
+echo -n "DIRECT"
 EOF
 
     # Assurez-vous que le script de détection de proxy est exécutable
